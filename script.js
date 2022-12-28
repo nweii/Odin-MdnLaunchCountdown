97864dff31b6fe783cdfee6e506d44fb4e594114
Nathan Cheng <35241373+nweii@users.noreply.github.com>
2022-12-28 15:04:13 +0000

    Completed assignment

--- script.js
+++ script.js
@@ -12,8 +12,21 @@
 let output = document.querySelector('.output');
 output.innerHTML = '';
 
-// let i = 10;
+let i = 10;
+while (i >= 0) {
+  const para = document.createElement('p');
+  if (i === 10) {
+    para.textContent = 'Counting 10';
+  }
+  else if (i === 0) {
+    para.textContent = 'Blast off!';
+  }
+  else {
+    para.textContent = i;
+  }
+  
+  output.appendChild(para);
+  
+  i--;
+}
 
-// const para = document.createElement('p');
-// para.textContent = ;
-// output.appendChild(para);
